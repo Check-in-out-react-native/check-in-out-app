7f6e494ee08aa786cdd9cac9d8c3387c9cc05bb5
viniciusmilanez <vinicius.254711@alunosatc.edu.br>
2024-06-17 19:48:11 -0300

    Ajustes cadastro mesas

--- src/components/ModalMesa/index.jsx
+++ src/components/ModalMesa/index.jsx
@@ -1,44 +1,39 @@
 
 import { style } from './style';
-import { validateNomeMesa, validateQntdLugares } from './validation';
+import {validateQntdLugares } from './validation';
 import { Modal, Button, Portal, Surface, IconButton, TextInput } from 'react-native-paper';
 import { Text } from 'react-native';
 import { useState, useContext } from 'react';
-import { fetchMesas } from '../../services';
+import { fetchAdicionaMesa } from '../../services';
 import { PrincipalContext } from '../../context/PrincipalProvider';
 
 const ModalMesa = ({ setVisible, visible }) => {
   const { setNotificacao, setPrincipal } = useContext(PrincipalContext);
   const [qntdLugares, setQntdLugares] = useState(0);
-  const [nome, setNome] = useState('');
   const [errors, setErrors] = useState({});
 
   const hideModal = () => {
-    setNome('');
     setErrors({});
     setQntdLugares(0);
     setVisible(false);
   };
 
   const cadastrarMesa = () => {
-    const nomeError = validateNomeMesa(nome);
-    const qntdLugares = validateQntdLugares(qntdLugares);
+    const qntdLugaresError = validateQntdLugares(qntdLugares);
+    
 
-    if (nomeError || qntdLugaresError) {
-      setErrors({ nome: nomeError, qntdLugares: qntdLugaresError });
+    if (qntdLugaresError) {
+      setErrors({ qntdLugares: qntdLugaresError });
       return;
     }
 
     const dto = {
-      nome_mesa: nome,
       qtd_lugares: qntdLugares
     };
 
     const cbSuccess = (data) => {
-      setPrincipal((prev) => ({
-       ...prev,
-        espera: [...prev.espera, { nome_cliente: nome, id_cliente: data.id_cliente }]
-      }));
+
+    
 
       hideModal();
 
@@ -57,7 +52,7 @@ const ModalMesa = ({ setVisible, visible }) => {
       hideModal();
     };
 
-    fetchMesas(dto, cbSuccess, cbError);
+    fetchAdicionaMesa(dto, cbSuccess, cbError);
   };
 
   return (
@@ -67,18 +62,12 @@ const ModalMesa = ({ setVisible, visible }) => {
           <Text style={{ fontSize: 20 }}>Cadastro de mesa</Text>
           <IconButton icon="close" onPress={hideModal} style={{ width: 20 }} />
         </Surface>
-        <TextInput
-          label="Nome"
-          value={nome}
-          onChangeText={(text) => setNome(text)}
-          error={errors?.nome}
-        />
-        {errors?.nome && <Text style={{ color: '#ac0000' }}>{errors?.nome}</Text>}
+  
         <TextInput
           label="Quantidade de lugares"
           value={qntdLugares}
           keyboardType="number-pad"
-          onChangeText={(text) => setQntdPessoas(text)}
+          onChangeText={(text) => setQntdLugares(text)}
           error={errors?.qntdLugares}
         />
         {errors?.qntdLugares && <Text style={{ color: '#ac0000' }}>{errors?.qntdLugares}</Text>}
